be9eb80d02604b25c5cce7c603b6272fcabe24ea
A-fethi <fethiabderrahmane1@gmail.com>
2023-03-17 06:14:59 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int	c;
+
+	c = '0';
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
