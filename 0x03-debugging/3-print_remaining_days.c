e14f516475510512f7ba77c8391b77e39bdc8ad3
A-fethi <fethiabderrahmane1@gmail.com>
2023-03-23 11:28:32 -0700

    HEADER_FILE

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,9 +10,9 @@
 * Return: void
 */
 
-void	print_remaining_days(int month, int day, int year)
+void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0) || (year % 400 == 0) && (year % 100 == 0))
+	if ((year % 4 == 0) || (year % 400 == 0 && year % 100 == 0))
 	{
 		if (month > 2 && day >= 60)
 		{
@@ -32,6 +32,7 @@ void	print_remaining_days(int month, int day, int year)
 		{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
+
 		}
 	}
 }
