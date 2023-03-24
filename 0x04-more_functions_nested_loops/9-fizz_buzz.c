ec655dcd3ce06128aebdbfeaff85247496a3acfd
A-fethi <fethiabderrahmane1@gmail.com>
2023-03-23 18:16:37 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on Succes.
+ */
+
+int main(void)
+{
+	int	i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
