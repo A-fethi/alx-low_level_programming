e3d8072d65d15921d3e788569f233e588b84542b
A-fethi <fethiabderrahmane1@gmail.com>
2023-03-29 08:19:57 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -21,13 +21,3 @@ int	_strcmp(char *s1, char *s2)
 	}
 	return (0);
 }
-int main(void)
-{
-    char s1[] = "Hello";
-    char s2[] = "World!";
-
-    printf("%d\n", _strcmp(s1, s2));
-    printf("%d\n", _strcmp(s2, s1));
-    printf("%d\n", _strcmp(s1, s1));
-    return (0);
-}
