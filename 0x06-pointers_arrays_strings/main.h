90b1f4608861f4043349b57e54f00083ed19612c
A-fethi <fethiabderrahmane1@gmail.com>
2023-03-29 12:50:28 -0700

    HEADER_FILE

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,12 +3,16 @@
 
 #include <stdio.h>
 
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
+int 	_putchar(char c);
+int	main(void);
+char	*_strcat(char *dest, char *src);
+char	*_strncat(char *dest, char *src, int n);
+char	*_strncpy(char *dest, char *src, int n);
+int	_strcmp(char *s1, char *s2);
+void	reverse_array(int *a, int n);
+char	*string_toupper(char *);
+char	*cap_string(char *);
+char	*rot13(char *);
+void	print_number(int n);
 
 #endif
