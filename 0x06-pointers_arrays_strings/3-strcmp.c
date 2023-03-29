a05cb303caa2f3d7f8aa0eee0f9a82780502afb4
A-fethi <fethiabderrahmane1@gmail.com>
2023-03-29 08:16:49 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,10 +15,20 @@ int	_strcmp(char *s1, char *s2)
 	while (s1[i] || s2[i])
 	{
 		if (s1[i] == s2[i])
-			return (0);
+			i++;
 		else
 			return (s1[i] - s2[i]);
 		i++;
 	}
 	return (0);
+}
+int main(void)
+{
+    char s1[] = "Hello";
+    char s2[] = "World!";
+
+    printf("%d\n", _strcmp(s1, s2));
+    printf("%d\n", _strcmp(s2, s1));
+    printf("%d\n", _strcmp(s1, s1));
+    return (0);
 }
