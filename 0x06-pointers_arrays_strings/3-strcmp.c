7a7f9f8eb5a8d0ba336ac5cad4c1e5914fb9d457
A-fethi <fethiabderrahmane1@gmail.com>
2023-03-29 07:18:51 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare 2 string
+ * @s1:first string
+ * @s2:second string
+ * Return:result of the comparaison
+ */
+
+int	_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] == s2[i])
+			return (0);
+		else
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
