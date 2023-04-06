e8fa41a4b9e716a55c8cf4509ab7cf43d7671281
A-fethi <fethiabderrahmane1@gmail.com>
2023-04-06 08:16:04 -0700

    Without libraries what have we? We have no past and no future

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
 gcc -c *.c
+
+# compile our library code into an object file using -c with GCC compiler.
+
 ar -rc liball.a *.o
-nm liball.a
+
+# ar for 'archiver' used to create static libraries, modify object files in the static library, list the names of object files in the library.
+# rc: The r flag tells ar to replace older object files in the library with the new object files. The c flag tells it to create the library if it doesn't already exist.
+# so the command ar -rc liball.a *.o creates a static library named liball.a and puts copies of all object files in it.
