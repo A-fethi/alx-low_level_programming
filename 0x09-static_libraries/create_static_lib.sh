3f0d3d06e7913949d17dd8f9486c2c1c40daaae0
A-fethi <fethiabderrahmane1@gmail.com>
2023-04-07 08:14:55 -0700

    Without libraries what have we? We have no past and no future

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-
 gcc -c *.c
-
-# compile our library code into an object file using -c with GCC compiler.
-
-ar rc liball.a *.o
-
-# ar for 'archiver' used to create static libraries, modify object files in the static library, list the names of object files in the library.
-# rc: The r flag tells ar to replace older object files in the library with the new object files. The c flag tells it to create the library if it doesn't already exist.
-# so the command ar -rc liball.a *.o creates a static library named liball.a and puts copies of all object files in it.
-
+ar -rc liball.a *.o
 ranlib liball.a
-
-# The ranlib command is typically used in conjunction with the ar command, which is used to create and manipulate library archives. After creating a library archive using the ar command, you can use ranlib to generate an index for the archive.
