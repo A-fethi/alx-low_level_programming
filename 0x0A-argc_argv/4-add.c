e39b610526d13cf3edee99ca06452a6d8aa709aa
A-fethi <fethiabderrahmane1@gmail.com>
2023-04-08 07:13:40 -0700

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -41,10 +41,7 @@ int main(int argc, char *argv[])
 				printf("Error\n");
 				return (1);
 			}
-			else
-			{
-				sum += atoi(argv[i]);
-			}
+			sum += atoi(argv[i]);
 			i++;
 		}
 		printf("%d\n", sum);
