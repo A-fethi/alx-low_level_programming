bc47de6b720cd36f5af7396e0092da642c4f7fe5
A-fethi <fethiabderrahmane1@gmail.com>
2023-04-17 10:02:37 -0700

    A dog is the only thing on earth that loves you more than you love yourself

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,37 +1,6 @@
 #include "dog.h"
 
 /**
- * _strdup - copies the str given as parameter to dest
- * @str: the string
- * Return: NULL if str is NULL, else pointer to the new string
- */
-
-char	*_strdup(char *str)
-{
-	int	i;
-	char	*dest;
-
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	i = 0;
-	while (str[i])
-		i++;
-	dest = malloc(sizeof(char) * (i + 1));
-	if (!dest)
-		return (NULL);
-	i = 0;
-	while (str[i])
-	{
-		dest[i] = str[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-/**
  * init_dog - initializes a variable of type struct dog
  * @d: variable
  * * @name: name
@@ -41,7 +10,9 @@ char	*_strdup(char *str)
 
 void	init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = _strdup(name);
+	if (!d)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
 	d->age = age;
-	d->owner = _strdup(owner);
+	d->owner = owner;
 }
