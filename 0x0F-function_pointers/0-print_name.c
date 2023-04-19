807a66cac0f57698f749c568ed6e92fdbe637327
A-fethi <fethiabderrahmane1@gmail.com>
2023-04-19 06:52:45 -0700

    What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,6 +3,12 @@
 /**
  * print_name - prints a name
  * @name: the name to print
- * @f: pointer to function 
+ * @f: pointer to function
  */
 
+void	print_name(char *name, void (*f)(char *))
+{
+	if (!f || !name)
+		return;
+	(*f)(name);
+}
