bfedf762993afeafec41823975d2564bf9b19582
A-fethi <fethiabderrahmane1@gmail.com>
2023-04-27 04:48:13 -0700

    HEADER FILE

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <stddef.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -14,13 +15,14 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
-size_t print_list(const list_t *h);
+size_t	print_list(const list_t *h);
 
 #endif
