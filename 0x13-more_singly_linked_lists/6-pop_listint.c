cd43dc58a46f01e10372afbd9f44bd21dae3a66f
A-fethi <fethiabderrahmane1@gmail.com>
2023-04-29 04:54:56 -0700

    Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,11 +9,13 @@
 int	pop_listint(listint_t **head)
 {
 	listint_t	*tmp;
+	int		data;
 
-	if (!head || !*head)
+	if (!*head)
 		return (0);
+	data = (*head)->n;
 	tmp = *head;
 	*head = (*head)->next;
 	free(tmp);
-	return (0);
+	return (data);
 }
