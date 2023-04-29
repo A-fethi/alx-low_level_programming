765b8da70fd383b4f79d59b98d574a4fb701c56d
A-fethi <fethiabderrahmane1@gmail.com>
2023-04-29 04:22:07 -0700

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,11 @@ void	free_listint2(listint_t **head)
 {
 	listint_t	*tmp;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
 	while (*head != NULL)
 	{
-		tmp = (*head)->next;
+		tmp = *head->next;
 		free(*head);
 		*head = tmp;
 	}
