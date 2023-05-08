6277e5f51e6bd6891f39e8744b5b6fda1176e7aa
A-fethi <fethiabderrahmane1@gmail.com>
2023-05-08 08:19:50 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int	create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 	{
-		write(fd, NULL, 0);
+		write(fd, text_content, strlen(text_content));
 	}
 	close(fd);
 	return (1);
