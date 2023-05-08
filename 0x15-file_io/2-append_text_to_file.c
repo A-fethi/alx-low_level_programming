38df1cbad917fbd62cfb8988f17fa67c6e837bc8
A-fethi <fethiabderrahmane1@gmail.com>
2023-05-08 09:55:41 -0700

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,9 +17,26 @@ int	append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	w = write(fd, text_content, strlen(text_content));
-	if (w == -1)
-		return (-1);
+	if (text_content != NULL)
+	{
+		w = write(fd, text_content, strlen(text_content));
+		if (w == -1)
+			return (-1);
+	}
 	close(fd);
 	return (1);
+}
+
+int main(int ac, char **av)
+{
+    int res;
+
+    if (ac != 3)
+    {
+        dprintf(2, "Usage: %s filename text\n", av[0]);
+        exit(1);
+    }
+    res = append_text_to_file(av[1], av[2]);
+    printf("-> %i)\n", res);
+    return (0);
 }
