f874f912d9c1d50ae6a19a7c4ca8cda55b9dcd1c
A-fethi <fethiabderrahmane1@gmail.com>
2023-06-19 11:22:07 -0700

    let's win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/A-fethi/alx-low_level_programming/raw/master/0x18-dynamic_libraries/inject.so
-export LD_PRELOAD=/tmp/inject.so
+wget -P /$PWD https://github.com/A-fethi/alx-low_level_programming/raw/master/0x18-dynamic_libraries/inject.so
+export LD_PRELOAD=/$PWD/inject.so
