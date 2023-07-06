eb12931418060b893587446a3b0cb441ca90cb61
A-fethi <fethiabderrahmane1@gmail.com>
2023-07-06 14:04:00 -0700

    what was that if doing there...

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,13 +13,11 @@ char	*hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int	index;
 	hash_node_t		*node;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (ht == NULL || key == NULL)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
-	if (index >= ht->size)
-		return (NULL);
 	node = ht->array[index];
-	if (node != NULL)
+	while (node != NULL)
 	{
 		if (strcmp(node->key, key) == 0)
 			return (node->value);
