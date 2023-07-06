e2c96e08c7a4e5e7a4d89fe8d629b4fe62b964a4
A-fethi <fethiabderrahmane1@gmail.com>
2023-07-06 11:23:35 -0700

    edited

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -27,8 +27,6 @@ int	hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			if (new_value == NULL)
 				return (0);
 			strcpy(new_value, temp->value);
-			strcat(new_value, " ");
-			strcat(new_value, value);
 			free(temp->value);
 			temp->value = new_value;
 			return (1);
