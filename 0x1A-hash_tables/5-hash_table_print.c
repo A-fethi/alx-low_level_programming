885f2149f43751d1674687ae053ecb9873658982
A-fethi <fethiabderrahmane1@gmail.com>
2023-07-06 13:17:19 -0700

    adding table print and adding checks to table get

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table.
+ * @ht: hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int	i;
+	unsigned char		printed;
+
+	i = 0;
+	printed = 0;
+	if (ht == NULL)
+		return;
+	printf("{");
+	while (i < ht->size)
+	{
+		if (ht->array[i])
+		{
+			if (printed == 1)
+				printf(", ");
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			printed = 1;
+		}
+		i++;
+	}
+	printf("}\n");
+}
