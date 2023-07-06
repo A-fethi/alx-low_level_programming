5d6c26a29e0aae764a7846595fb872f7bd7f4243
A-fethi <fethiabderrahmane1@gmail.com>
2023-07-06 12:44:20 -0700

    Adding more errors check

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,7 +13,8 @@ char	*hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int	index;
 	hash_node_t		*node;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (ht == NULL || key == NULL || ht->size == 0 ||
+			ht->array == NULL || strlen(key) == 0)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
