b0f8225560fa0c3c8092b725900f3af2f1cbca24
A-fethi <fethiabderrahmane1@gmail.com>
2023-10-10 05:30:01 -0700

    HEADER FILE

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,8 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int	linear_search(int *array, size_t size, int value);
 int	binary_search(int *array, size_t size, int value);
+int	jump_search(int *array, size_t size, int value);
+int	interpolation_search(int *array, size_t size, int value);
 
 #endif
